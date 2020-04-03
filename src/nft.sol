03a0bfe401b8eb5260ebf72cd9f7c9944b4d7d69
vedhavyas <vedhavyas.singareddi@gmail.com>
2020-04-03 17:39:47 +0530

    revert functions and test

--- src/nft.sol
+++ src/nft.sol
@@ -13,30 +13,156 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity >=0.4.24;
+pragma solidity >=0.5.15 <0.6.0;
 pragma experimental ABIEncoderV2;
 
 import { ERC721Metadata } from "./openzeppelin-solidity/token/ERC721/ERC721Metadata.sol";
-import "./asset_verifier.sol";
+import "./openzeppelin-solidity/cryptography/ECDSA.sol";
 
-contract NFT is ERC721Metadata, AssetVerifier {
+contract AssetManagerLike {
+    function isAssetValid(bytes32 asset) external view returns (bool);
+}
 
-    constructor(string memory name, string memory symbol, address _assetManager) ERC721Metadata(name, symbol) public {
-        assetManager = IAssetManager(_assetManager);
+contract KeyManagerLike {
+    function keyHasPurpose(bytes32, uint) public view returns (bool);
+}
+
+contract IdentityFactoryLike {
+    function createdIdentity(address) public view returns (bool);
+}
+
+contract NFT is ERC721Metadata {
+
+    using ECDSA for bytes32;
+
+    // compact prop for "nfts"
+    bytes constant internal NFTS = hex"0100000000000014";
+    // Value of the Signature purpose for an identity. sha256('CENTRIFUGE@SIGNING')
+    // solium-disable-next-line
+    uint constant internal SIGNING_PURPOSE = 0x774a43710604e3ce8db630136980a6ba5a65b5e6686ee51009ed5f3fded6ea7e;
+
+    AssetManagerLike assetManager;
+    IdentityFactoryLike identityFactory;
+    KeyManagerLike keyManager;
+
+    constructor(string memory name, string memory symbol, address _assetManager, address _identityFactory, address _keyManager) ERC721Metadata(name, symbol) public {
+        assetManager = AssetManagerLike(_assetManager);
+        identityFactory = IdentityFactoryLike(_identityFactory);
+        keyManager = KeyManagerLike(_keyManager);
     }
 
     event Minted(address to, uint tokenId);
 
-  /**
-   * @dev Mints a token to a specified address
-   * @param to address deposit address of token
-   * @param tokenId uint256 tokenID
-   */
-    function mint(address to, uint256 tokenId, bytes[] memory properties, bytes[] memory values, bytes32[] memory salts) public {
-        require(_verify_asset(to, properties, values, salts), "asset hash invalid");
+    /**
+     * @dev Mints a token to a specified address
+     * @param to address deposit address of token
+     * @param tokenId uint256 tokenID
+     */
+    function _mint(address to, uint256 tokenId) internal {
         super._mint(to, tokenId);
         emit Minted(to, tokenId);
     }
 
+    /**
+     * Computes the asset hash from the given params and verifies if its present in the AssetManager
+     * @param to address deposit address of the token
+     * @param properties bytes[] properties of the each proof
+     * @param values bytes[] value associated with each property
+     * @param salts bytes[] salt associated with each property
+     */
+    function _verify_asset(address to, bytes[] memory properties, bytes[] memory values, bytes32[] memory salts) internal view returns (bool) {
+        require(to != address(0), "not a valid address");
+
+        // construct assetHash from the props, values, salts
+        // append to address
+        bytes memory data = abi.encodePacked(to);
+
+        // append hashes
+        for (uint i=0; i< properties.length; i++) {
+            data = abi.encodePacked(data, keccak256(abi.encodePacked(properties[i], values[i], salts[i])));
+        }
+
+        return assetManager.isAssetValid(keccak256(data));
+    }
+
+    /**
+     * @dev Checks that provided document is signed by the given identity. Validates and checks if the public key used is a valid SIGNING_KEY.
+     * Does not check if the signature root is part of the document root.
+     * @param data_root bytes32 hash of all data fields of the document which are signed
+     * @param signature bytes The signature used to sign the data root
+     */
+    function _signed(bytes32 data_root, bytes memory signature) internal view {
+        // Extract the public key and identity address from the signature
+        address identity_ = data_root.toEthSignedMessageHash().recover(signature);
+        bytes32 pbKey_ = bytes32(uint(identity_) << 96);
+        // check that the identity being used has been created by the Centrifuge Identity Factory contract
+        require(identityFactory.createdIdentity(identity_), "Identity is not registered.");
+        // check that public key has signature purpose on provided identity
+        require(keyManager.keyHasPurpose(pbKey_, SIGNING_PURPOSE), "Signature key is not valid.");
+    }
+
+    /**
+     * @dev Checks that the passed in token proof matches the data for minting
+     * @param tkn uint The ID for the token to be minted
+     */
+    function _checkTokenData(uint tkn, bytes memory property, bytes memory value) internal view {
+        require(_bytesToUint(value) == tkn, "tokenID mismatch");
+        require(_equalBytes(property, abi.encodePacked(NFTS, address(this), hex"000000000000000000000000")), "token property mismatch");
+    }
+
+    /**
+     * @dev Parses bytes and extracts a uint value
+     * @param data bytes From where to extract the index
+     * @return result the converted address
+     */
+    function _bytesToUint(bytes memory data) internal pure returns (uint) {
+        require(data.length <= 256, "slicing out of range");
+        return abi.decode(data, (uint));
+    }
+
+    function _equalBytes(bytes memory a, bytes memory b) internal pure returns (bool) {
+        if (a.length != b.length) {
+            return false;
+        }
+
+        for (uint i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @dev Parses a uint and returns the hex string
+     * @param payload uint
+     * @return string the corresponding hex string
+     */
+    function uintToHexStr(uint payload) internal pure returns (string memory) {
+        if (payload == 0)
+            return "0";
+        // calculate string length
+        uint i = payload;
+        uint length;
+
+        while (i != 0) {
+            length++;
+            i = i >> 4;
+        }
+        // parse byte by byte and construct the string
+        i = payload;
+        uint mask = 15;
+        bytes memory result = new bytes(length);
+        uint k = length - 1;
+
+        while (i != 0) {
+            uint curr = (i & mask);
+            result[k--] = curr > 9 ? byte(55 + uint8(curr)) : byte(48 + uint8(curr));
+            i = i >> 4;
+        }
+
+        return string(result);
+    }
 }
 
--- src/test/nft.t.sol
+++ src/test/nft.t.sol
@@ -13,55 +13,99 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity >=0.4.23;
+pragma solidity >=0.5.15 <0.6.0;
 pragma experimental ABIEncoderV2;
 
 import "../nft.sol";
 import "../../lib/ds-test/src/test.sol";
 
-contract MockAssetManager {
+contract AssetManagerMock {
     mapping (bytes32 => uint8) public assets;
+    bool assetValid;
 
-    function store(bytes32 asset) public {
-        require(assets[asset] != 1, "Asset cannot be changed once confirmed");
-        assets[asset] = 1;
+    function file(bool assetVaild_) public {
+        assetValid = assetVaild_;
     }
 
     function isAssetValid(bytes32 asset) external view returns (bool) {
-        if (assets[asset] == 1) {
-            return true;
-        }
+        return assetValid;
+    }
+}
 
-        return false;
+contract KeyManagerMock {
+    bytes32 key;
+    bytes32 value;
+    uint purpose;
+    uint[] mem;
+    bool validPurpose;
+    uint32 revoked;
+
+    function file(bool validity_) public {
+        validPurpose = validity_;
+    }
+
+    function keyHasPurpose(bytes32 pbKey, uint purpose_) public view returns (bool) {
+        return validPurpose;
+    }
+}
+
+contract IDFactoryMock {
+    address identity;
+    bool validIdentity;
+
+    function file(bool validity_) public {
+        validIdentity = validity_;
+    }
+
+    function createdIdentity(address identity_) public returns (bool) {
+        return validIdentity;
+    }
+}
+
+contract TestNFT is NFT {
+    constructor (address asset_manager_, address key_manager_, address identity_factory_) NFT("Test NFT", "TNFT", asset_manager_, key_manager_, identity_factory_) public {
+    }
+
+    /**
+    @dev Mints NFT after verifying the asset, signture of the collaborator and token uniqueness
+    */
+    function mint(address usr, uint tkn, bytes32 data_root, bytes[] memory properties, bytes[] memory values, bytes32[] memory salts) public {
+        require(_verify_asset(usr, properties, values, salts), "asset hash invalid");
+        _signed(data_root, values[0]); // expect the first value to be collaborator signature
+        _checkTokenData(tkn, properties[1], values[1]); // expects the second property and value to be token unique proof
+        _mint(usr, tkn);
     }
 }
 
 contract NFTTest is DSTest {
-    NFT nft;
-    MockAssetManager assetManager;
-    uint256 tokenId;
+    TestNFT nft;
+    AssetManagerMock assetManager;
+    KeyManagerMock keyManager;
+    IDFactoryMock identityFactory;
+    uint tokenId;
+    bytes32 dataRoot;
     address to;
-    bytes32 assetHashValid;
-    bytes32 assetHashInvalid;
     bytes[] props;
     bytes[] values;
     bytes32[] salts;
     bytes32[] saltsInvalid;
 
     function setUp() public {
-        assetManager = new MockAssetManager();
-        nft = new NFT("NFT", "NFT", address(assetManager));
-        tokenId = uint256(keccak256(hex"000200000000000c"));
-        to = bytesToAddress(hex"f2bd5de8b57ebfc45dcee97524a7a08fccc80aef");
-        assetHashValid = 0xee49e1ca6aa1204cfb571094ce14ab254e5185005cbee3f26af9afd3140ac12d;
+        assetManager = new AssetManagerMock();
+        identityFactory = new IDFactoryMock();
+        keyManager = new KeyManagerMock();
+        nft = new TestNFT(address(assetManager), address(identityFactory), address(keyManager));
+        tokenId = 1;
+        to = address(1234);
+        dataRoot = 0xca87e9ba4fcfc9eb27594e18d14dc3fb094913e67c9aa3f19e0e3205dbb7dbfa;
         props = new bytes[](3);
         props[0] = hex"392614ecdd98ce9b86b6c82242ae1b85aaf53ebe6f52490ed44539c88215b17a";
-        props[1] = hex"8db964a550ede5fea3f059ca6a74cf436890bb1d31a39c63ea0ccfbc8d8235fd";
-        props[2] = hex"c437005805629feeb716f4ff329f62a4cf393f4cbfc7cd14fc0a64d8321a3e99";
+        props[1] = hex"0100000000000014e821d1b50945ff736992d0af793684dd53ac7fa7000000000000000000000000";
+        props[2] = hex"000100000000000d";
 
         values = new bytes[](3);
-        values[0] = hex"d6ad85800460ea404f3289484f9300ed787dc951203cb3f0ef5fa0fa4db283cc";
-        values[1] = hex"446bfed759680364b759d32d6d217e287df7aad0bf4c82816f124d7e03ab248f";
+        values[0] = hex"a2776063c2177a8e4be999fd337d939d03df0f341c50d2dac45dafad0008016e248cfb0076035c514dfc66af39e574bcc795a6af6b112a6ec90ff9291c766b7c01";
+        values[1] = hex"fc03d8fc2094952d153396f1904513850b4f76fcfeaef9c44dcb6d7de1921674";
         values[2] = hex"443e4fa3d89952c9f24433d1112713a075d9205195dc9a16a12301caa1afb5d2";
 
         salts = new bytes32[](3);
@@ -75,37 +119,60 @@ contract NFTTest is DSTest {
         saltsInvalid[2] = 0xed58f4a0d0c76770c81d2b1cc035413edebb567f5c006160596dc73b9297a9cd;
     }
 
-    function bytesToAddress(bytes memory bys) private pure returns (address addr) {
-        assembly {
-            addr := mload(add(bys,20))
-        }
-    }
-
     function testFailInvalidToAddress() public logs_gas {
-        nft.mint(address(0), tokenId, props, values, salts);
-        assertEq(address(0), nft.ownerOf(tokenId));
+        nft.mint(address(0), tokenId, dataRoot, props, values, salts);
     }
 
     function testFailMismatchAssetHash() public logs_gas {
-        nft.mint(to, tokenId, props, values, saltsInvalid);
-        assertEq(nft.balanceOf(to), 0);
+        nft.mint(to, tokenId, dataRoot, props, values, saltsInvalid);
     }
 
-    function testSuccessNFTMint() public logs_gas {
-        assetManager.store(assetHashValid);
-        nft.mint(to, tokenId, props, values, salts);
+    function testFailIdentityNotRegistered() public logs_gas {
+        assetManager.file(true);
+        nft.mint(to, tokenId, dataRoot, props, values, salts);
+    }
+
+    function testFailPublicKeyNotValid() public logs_gas {
+        assetManager.file(true);
+        identityFactory.file(true);
+        nft.mint(to, tokenId, dataRoot, props, values, salts);
+    }
+
+    function testFailTokenIDValueMismatch() public logs_gas {
+        assetManager.file(true);
+        identityFactory.file(true);
+        keyManager.file(true);
+        nft.mint(to, tokenId, dataRoot, props, values, salts);
+    }
+
+    function testFailTokenPropertyMismatch() public logs_gas {
+        assetManager.file(true);
+        identityFactory.file(true);
+        keyManager.file(true);
+        values[1] = hex"0000000000000000000000000000000000000000000000000000000000000001";
+        nft.mint(to, tokenId, dataRoot, props, values, salts);
+    }
+
+    function testSuccessMintNFT() public logs_gas {
+        assetManager.file(true);
+        identityFactory.file(true);
+        keyManager.file(true);
+        values[1] = hex"0000000000000000000000000000000000000000000000000000000000000001";
+        props[1] = abi.encodePacked(hex"0100000000000014", address(nft), hex"000000000000000000000000");
+        nft.mint(to, tokenId, dataRoot, props, values, salts);
         assertEq(nft.balanceOf(to), 1);
         assertEq(nft.ownerOf(tokenId), to);
     }
 
-    function testFailNFTOverride() public logs_gas {
-        assetManager.store(assetHashValid);
-        nft.mint(to, tokenId, props, values, salts);
+    function testFailDoubleMintNFT() public logs_gas {
+        assetManager.file(true);
+        identityFactory.file(true);
+        keyManager.file(true);
+        values[1] = hex"0000000000000000000000000000000000000000000000000000000000000001";
+        props[1] = abi.encodePacked(hex"0100000000000014", address(nft), hex"000000000000000000000000");
+        nft.mint(to, tokenId, dataRoot, props, values, salts);
         assertEq(nft.balanceOf(to), 1);
         assertEq(nft.ownerOf(tokenId), to);
-
-        // mint again
-        nft.mint(to, tokenId, props, values, salts);
-        assertEq(nft.balanceOf(to), 1);
+        nft.mint(to, tokenId, dataRoot, props, values, salts);
     }
 }
